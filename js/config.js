8ba94429dee06f4beaf9a9cd1be1e76a0d000971
Ben Shaw <ben.shaw@clearscore.com>
2026-07-06 22:28:49 +0100

    feat: project scaffold (static shell, css, config stub)

---
+++ js/config.js
@@ -0,0 +1,7 @@
+// Paste your Firebase web app config here (Task 2).
+export const firebaseConfig = {
+  apiKey: 'REPLACE_ME',
+  authDomain: 'REPLACE_ME.firebaseapp.com',
+  databaseURL: 'https://REPLACE_ME-default-rtdb.firebaseio.com',
+  projectId: 'REPLACE_ME',
+};
