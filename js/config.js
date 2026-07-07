9d6120d63f675b4aa775b76ca2b17ab8a133be62
Ben Shaw <ben.shaw@clearscore.com>
2026-07-07 07:00:51 +0100

    feat: real firebase project config (dnd-traverse)

--- js/config.js
+++ js/config.js
@@ -1,7 +1,10 @@
-// Paste your Firebase web app config here (Task 2).
+// Firebase web app config (public-by-design identifiers, safe to commit).
 export const firebaseConfig = {
-  apiKey: 'REPLACE_ME',
-  authDomain: 'REPLACE_ME.firebaseapp.com',
-  databaseURL: 'https://REPLACE_ME-default-rtdb.firebaseio.com',
-  projectId: 'REPLACE_ME',
+  apiKey: 'AIzaSyC5h5SsKlNk_yQieNwQv4ho3kjdI7I7llk',
+  authDomain: 'dnd-traverse.firebaseapp.com',
+  databaseURL: 'https://dnd-traverse-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'dnd-traverse',
+  storageBucket: 'dnd-traverse.firebasestorage.app',
+  messagingSenderId: '193816243991',
+  appId: '1:193816243991:web:545d20a20e6a76c2075152',
 };
